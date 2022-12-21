272844434da3448e59ba73adf458da8e52813bdd
maggie197 <megijagabalina@gmail.com>
2022-12-21 21:38:25 +0000

    added flash message

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, flash
 from markupsafe import escape
 
 views = Blueprint('views', __name__)
@@ -10,4 +10,5 @@ def index():
         username = session["username"]
         return render_template('index.html', username=username)
     else:
+        flash("You are not logged in!", "error")
         return redirect(url_for("auth.login"))
