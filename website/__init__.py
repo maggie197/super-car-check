5bf7f437afd23ac5c4670afd3438f83ee7feb704
maggie197 <megijagabalina@gmail.com>
2022-12-21 18:03:39 +0000

    imported blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -6,5 +6,12 @@ from flask import Flask
 def create_app():        #set a function inisializing flask
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'thisisasecretkey!'# to secure cookies and sesssion data
-    
+
+# imported blueprints
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
