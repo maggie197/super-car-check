9ddff74746eecc33ce0dda5457cd7fe291949924
maggie197 <megijagabalina@gmail.com>
2022-12-21 18:20:45 +0000

    added login route

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/home')
-def homepage():
-    return 'Hello, World'
+
+@auth.route('/login')
+def login():
+    return 'login'
