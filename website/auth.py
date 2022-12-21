fc7921664d2fb180985167b8fb9b0c2dd08026c0
maggie197 <megijagabalina@gmail.com>
2022-12-21 19:05:47 +0000

    updated login page with request data

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return 'index'()
-    else:
-        return 'login'()
+        if (request.form['username'],
+           request.form['password']):
+            return (request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
