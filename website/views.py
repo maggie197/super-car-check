771b870cdd46029058fdbb46fc69cc7ab404ae7a
maggie197 <megijagabalina@gmail.com>
2022-12-21 18:12:51 +0000

    added variable sections to a URL

--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
+from markupsafe import escape
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return "<h1> Hello </h1>"
+
+@views.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
