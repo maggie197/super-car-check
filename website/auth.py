761c613f76938a635eadf1a714fc7832bc3dfd27
maggie197 <megijagabalina@gmail.com>
2022-12-21 21:15:02 +0000

    added flash message

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 auth = Blueprint('auth', __name__)
 
 
@@ -8,7 +8,7 @@ def login():
         username = request.form["username"]
         session["username"] = username
         return redirect(url_for("views.index", username=username))
-
+        
     else:
         if "username" in session:
             return redirect(url_for("views.index"))
@@ -16,5 +16,9 @@ def login():
 
 @auth.route('/logout')
 def logout():
+    if "username" in session:
+        username = session["username"]
+        flash(f"You have been logged out, {username}", "info")
+
     session.pop("username", None)
     return redirect(url_for("auth.login"))
